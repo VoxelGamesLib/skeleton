1da9a4d82a48c639b05a46f9a3ec2544467335e3
MiniDigger <admin@minidigger.me>
2018-02-11 00:07:38 +0100

    add missing feature info annotation

--- src/main/java/me/minidigger/voxelgameslib/skeleton/SkeletonFeature.java
+++ src/main/java/me/minidigger/voxelgameslib/skeleton/SkeletonFeature.java
@@ -3,7 +3,9 @@ package me.minidigger.voxelgameslib.skeleton;
 import javax.annotation.Nonnull;
 
 import com.voxelgameslib.voxelgameslib.feature.AbstractFeature;
+import com.voxelgameslib.voxelgameslib.feature.FeatureInfo;
 
+@FeatureInfo(name = "SkeletonFeature", author = "MiniDigger", description = "SkeletonFeature description", version = "1.0.0")
 public class SkeletonFeature extends AbstractFeature {
 
     @Override
