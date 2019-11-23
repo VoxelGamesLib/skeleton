3689e7ba9c9e24e3c0346316c6f53cc2dc35a9dd
MiniDigger <admin@minidigger.me>
2019-11-23 21:02:46 +0100

    refactor stuff

--- src/main/java/me/minidigger/voxelgameslib/skeleton/SkeletonFeature.java
+++ src/main/java/me/minidigger/voxelgameslib/skeleton/SkeletonFeature.java
@@ -4,9 +4,9 @@ import java.util.Collections;
 import java.util.List;
 import javax.annotation.Nonnull;
 
-import com.voxelgameslib.voxelgameslib.feature.AbstractFeature;
-import com.voxelgameslib.voxelgameslib.feature.Feature;
-import com.voxelgameslib.voxelgameslib.feature.FeatureInfo;
+import com.voxelgameslib.voxelgameslib.api.feature.AbstractFeature;
+import com.voxelgameslib.voxelgameslib.api.feature.Feature;
+import com.voxelgameslib.voxelgameslib.api.feature.FeatureInfo;
 
 @FeatureInfo(name = "SkeletonFeature", author = "MiniDigger", description = "SkeletonFeature description", version = "1.0.0")
 public class SkeletonFeature extends AbstractFeature {
